6f149ec28a4796d1057dec06b2597de2274a920a
Lorenzo Navarro <lorenzojrnavarro@outlook.com>
2021-07-22 16:01:17 -0700

    Recipe App Upload
    Initial upload for the recipe app.

---
+++ Recipe App/Assets/Scripts/NonBlockingDropdown.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class NonBlockingDropdown : TMP_Dropdown
+{
+    protected override GameObject CreateBlocker(Canvas rootCanvas)
+    {
+        return null;
+    }
+}
---
+++ Recipe App/Assets/Scripts/SearchableDropdownController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(TMP_InputField))]
+public class SearchableDropdownController : MonoBehaviour
+{
+    public Action<SearchableDropdownController> ValueSelected;
+
+    [HideInInspector] public TMP_InputField inputField;
+    [HideInInspector] public NonBlockingDropdown dropdown;
+
+    private List<string> completeOptionsList;
+    private List<string> searchingList;
+
+    private void Awake()
+    {
+        inputField = GetComponent<TMP_InputField>();
+        dropdown = GetComponentInChildren<NonBlockingDropdown>();
+
+        inputField.onValueChanged.AddListener(UpdateOptions);
+        dropdown.onValueChanged.AddListener(UpdateInputFieldText);
+    }
+
+    private void RefreshOptions()
+    {
+        if (dropdown is null) return;
+        
+        dropdown.Hide();
+        dropdown.ClearOptions();
+        dropdown.AddOptions(searchingList);
+        dropdown.RefreshShownValue();
+    }
+
+    public void SetSearchableList(List<string> listItems)
+    {
+        for (int i = 0; i < listItems.Count; i++)
+        {
+            listItems[i] = Regex.Replace(listItems[i], "(\\B[A-Z])", " $1");
+        }
+
+        completeOptionsList = listItems;
+        searchingList = listItems;
+
+        RefreshOptions();
+    }
+
+    private void UpdateOptions(string text)
+    {
+        searchingList = completeOptionsList.FindAll((x) => Regex.IsMatch(x, Regex.Escape(text), RegexOptions.IgnoreCase));
+
+        //-1 works here because there is a valid placeholder
+        dropdown.SetValueWithoutNotify(-1);
+
+        RefreshOptions();
+        dropdown.Show();
+        inputField.Select();
+        inputField.selectionAnchorPosition = 1;
+    }
+
+    public void UpdateInputFieldText(int index)
+    {
+        inputField.SetTextWithoutNotify(searchingList[index]);
+        dropdown.SetValueWithoutNotify(index);
+
+        ValueSelected?.Invoke(this);
+    }
+
+    public string GetValue()
+    {
+        return Regex.Replace(inputField.text, "(\\s+)", "");
+    }
+
+    public void SetValue(string value)
+    {
+        inputField.text = Regex.Replace(value, "(\\B[A-Z])", " $1");
+        
+        dropdown.Hide();
+    }
+
+    private void OnDestroy()
+    {
+        inputField.onValueChanged.RemoveListener(UpdateOptions);
+        dropdown.onValueChanged.RemoveListener(UpdateInputFieldText);
+    }
+}
