6e120e8d383fc94a7bf8fb065de180dd86b21a54
angel-Jojo <87914878+angel-Jojo@users.noreply.github.com>
2021-08-09 19:39:10 -0700

    Scene Change: Main<->Add

---
+++ Recipe App/Assets/Scripts/goBack.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class goBack : MonoBehaviour
+{
+    public void backToMain()
+    {
+        SceneManager.LoadScene(0);
+    }
+        
+}
