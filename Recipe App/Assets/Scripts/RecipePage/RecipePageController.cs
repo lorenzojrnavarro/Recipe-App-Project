7d1563ca40e714dde89ae93a901bdb0a3f377c55
Lorenzo Navarro <lorenzojrnavarro@outlook.com>
2021-08-13 01:32:26 -0700

    Recipe Page Changes
    Recipe Page now takes an image from a URL

--- Recipe App/Assets/Scripts/RecipePage/RecipePageController.cs
+++ Recipe App/Assets/Scripts/RecipePage/RecipePageController.cs
@@ -6,12 +6,15 @@ using System.Linq;
 using System.Net.Http;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class RecipePageController : MonoBehaviour
 {
     public TextMeshProUGUI nameText;
     public TextMeshProUGUI ingredientsText;
     public TextMeshProUGUI directionsText;
+    public RawImage foodImage;
 
     private async void Awake()
     {
@@ -27,6 +30,8 @@ public class RecipePageController : MonoBehaviour
             nameText.text = recipes[0].Name;
             ingredientsText.text = string.Join("<br>", recipes[0].Ingredients);
             directionsText.text = string.Join("<br><br>", recipes[0].Directions);
+
+            StartCoroutine(LoadImage(foodImage, recipes[0].ImageURI));
         }
         catch(Exception e)
         {
@@ -34,5 +39,13 @@ public class RecipePageController : MonoBehaviour
         }
 
         client.Dispose();
+    }
+
+    private IEnumerator LoadImage(RawImage image, string imageURL)
+    {
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(imageURL);
+        yield return request.SendWebRequest();
+        Texture2D texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+        image.texture = texture;
     }
 }
